c08be0c8f5fb60accd2d90d641468ad75846513c
nsok4575-a11y <nsok4575@gmail.com>
2026-03-30 15:10:39 +0700

    Update Script The Spike Velleyball.By SNV THESPIKE.lua

--- Script The Spike Velleyball.By SNV THESPIKE.lua
+++ Script The Spike Velleyball.By SNV THESPIKE.lua
@@ -257,3 +257,16 @@ else
   gg.toast("✅ កែប្រែរួចរាល់")
 end
 end
+if start12 == 3 then
+  gg.setRanges(gg.REGION_C_ALLOC)
+
+  local Ball = gg.multiChoice(
+    {"WS", "MB", "SE", "🔙 Back"},
+    nil,
+    title
+  )
+
+  if Ball == nil then
+    start()
+    return
+    end
