2d8accb0d59d8f13668cb5563f31e20fb3da924e
nsok4575-a11y <nsok4575@gmail.com>
2026-07-22 21:13:30 +0700

    Update Script By SNV THESPIKE.lua

--- Script By SNV THESPIKE.lua
+++ Script By SNV THESPIKE.lua
@@ -139,7 +139,42 @@ local title = "Script THE Spike Volleyball\n📌 Version: " .. v ..
 
   if start12 == nil then return end
 if start12 == 1 then
-    
+
+    gg.clearResults()
+    gg.setRanges(gg.REGION_OTHER)
+
+    -- ស្វែងរក -1800 (DOUBLE)
+    gg.searchNumber("-1800", gg.TYPE_DOUBLE)
+
+    local results = gg.getResults(1000)
+    local editList = {}
+
+    for i, v in ipairs(results) do
+        local check = gg.getValues({
+            {
+                address = v.address + 0x20,
+                flags = gg.TYPE_DOUBLE
+            }
+        })
+
+        if check[1].value == -1 then
+            table.insert(editList, {
+                address = check[1].address,
+                flags = gg.TYPE_DOUBLE,
+                value = 0
+            })
+        end
+    end
+
+    if #editList > 0 then
+        gg.setValues(editList)
+        gg.toast("Edited: " .. #editList)
+    else
+        gg.toast("No matching values found")
+    end
+
+    gg.clearResults()
+        end
   
 if start12 == 2 then
 gg.setRanges(gg.REGION_C_ALLOC)
